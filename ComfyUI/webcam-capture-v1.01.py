1338bdf2dcd4c6ecabd7108a896b697b6b02021a
ReidDotO <83422507+ReidDotO@users.noreply.github.com>
2024-03-15 13:36:27 -0500

    Added Webcam Capture

---
+++ ComfyUI/webcam-capture-v1.01.py
@@ -0,0 +1,28 @@
+import cv2 
+
+key = cv2. waitKey(1)
+webcam = cv2.VideoCapture(0)
+while True:
+    try:
+        check, frame = webcam.read()
+        cv2.imshow("Capturing", frame)
+        cv2.imwrite(filename='capture.jpg', img=frame)
+        #print("Image saved!")
+        key = cv2.waitKey(1)
+        
+        if key == ord('q'):
+            print("Turning off camera.")
+            webcam.release()
+            print("Camera off.")
+            print("Program ended.")
+            cv2.destroyAllWindows()
+            break
+        
+    except(KeyboardInterrupt):
+        print("Turning off camera.")
+        webcam.release()
+        print("Camera off.")
+        print("Program ended.")
+        cv2.destroyAllWindows()
+        break
+    
